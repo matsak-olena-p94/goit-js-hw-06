9db7a2702aed3e6875d27e6e86527ded5ab1a87e
Olena Matsak <161434803+matsak-olena-p94@users.noreply.github.com>
2024-06-21 08:20:48 +0300

    add twoo

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,7 @@
 class Storage {
   #items;
   constructor(items) {
-    this.#items - items;
+    this.#items = items;
   }
   getItems() {
 return this.#items;
@@ -12,11 +12,7 @@ return this.#items;
   }
   
   removeItem(itemToRemove) {
-    const index = this.#items.indexOf(itemToRemove);
-    if (index !== -1) {
-      this.#items.splice(index, 1);
-    }
-    // this.#items = this.#items.filter(item => item !== itemToRemove)
+    this.#items = this.#items.filter(item => item !== itemToRemove)
   }
 }
 
